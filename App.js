6e13650db84f0bc4e33dc28fb0831933cd34d76e
krstkaan <k.kaanakbulak@gmail.com>
2024-10-06 02:37:23 +0300

    Added main categories and their respective subcategories and products.

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomePage from './.expo/screens/HomePage';
+import SubCategoryPage from './.expo/screens/SubCategoryPage';
+import ProductPage from './.expo/screens/ProductPage';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomePage" component={HomePage} />
+        <Stack.Screen name="SubCategoryPage" component={SubCategoryPage} />
+        <Stack.Screen name="ProductPage" component={ProductPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
